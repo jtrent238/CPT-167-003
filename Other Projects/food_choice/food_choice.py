1f86843bc083e2b819eb4b45e6ac85505ffc8a9f
Trent Patterson <jtrent238@outlook.com>
2018-10-10 15:09:00 -0400

    10102018308

--- Other Projects/food_choice/food_choice.py
+++ Other Projects/food_choice/food_choice.py
@@ -26,43 +26,35 @@ print("4. Hot Dog ................ 3.99");
 print("================================");
 
 #Asks the user to input a number 1-4
-ask_order = input("Please enter a number 1 - 5: ");
+ask_order = input("Please enter a number 1 - 4: ");
 
-# def menu1(argument):
-    # item = {
-        # 1: "Pizza",
-        # 2: "Chicken Fingers",
-        # 3: "Cheeseburger",
-        # 4: "Hot Dog"
-    # }
-# def menu2(argument):
-	# cost = {
-		# 1: "4.99",
-		# 2: "6.99",
-		# 3: "5.99",
-		# 3: "3.99"
-	# }
-
-# items = {
-		# 1: "Pizza",
-        # 2: "Chicken Fingers",
-        # 3: "Cheeseburger",
-        # 4: "Hot Dog"
-# }
-# print(menu2[ask_order]);
-# print menu1.get(argument, "Invalid number")
-# #print(item.get(argument), str) cost.get(argument), str);
+#Prints the output based on the number entered
+if ask_order in '1':
+	print("Your " + name1 + " meal will be ready soon.  Meal cost $" + cost1);
+elif ask_order in '2':
+	print("Your " + name2 + " meal will be ready soon.  Meal cost $" + cost2);
+elif ask_order in '3':
+    print("Your " + name3 + " meal will be ready soon.  Meal cost $" + cost3);
+elif ask_order in '4':
+    print("Your " + name4 + " meal will be ready soon.  Meal cost $" + cost4);
+else:
+	# Do the default
+	print("You have entered a number that is not between 1 and 4. Please enter a number between 1 and 4.");
+	
+	#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$
+	#Here is what I had if you want to run this just uncomment below and comment above.
+	#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$#$
 	
 #Prints the output based on the number entered
-if ask_order <=0:
-	print("You have entered a number that is less than 1. Please enter a number greater than 1.");
-if ask_order == 1:
-	print("Your " + name1 + " meal will be ready soon.  Meal cost " + cost1);
-if ask_order == 2:
-	print("Your " + name2 + " meal will be ready soon.  Meal cost " + cost2);
-if ask_order == 3:
-	print("Your " + name3 + " meal will be ready soon.  Meal cost " + cost3);
-if ask_order == 4:
-	print("Your " + name4 + " meal will be ready soon.  Meal cost " + cost4);
-if ask_order >= 5:
-	print("You have entered a number that is larger than 4. Please enter a number less than 4.");
+# if ask_order <=0:
+	# print("You have entered a number that is less than 1. Please enter a number greater than 1.");
+# if ask_order == 1:
+	# print("Your " + name1 + " meal will be ready soon.  Meal cost " + cost1);
+# if ask_order == 2:
+	# print("Your " + name2 + " meal will be ready soon.  Meal cost " + cost2);
+# if ask_order == 3:
+	# print("Your " + name3 + " meal will be ready soon.  Meal cost " + cost3);
+# if ask_order == 4:
+	# print("Your " + name4 + " meal will be ready soon.  Meal cost " + cost4);
+# if ask_order >= 5:
+	# print("You have entered a number that is larger than 4. Please enter a number less than 4.");
--- Other Projects/food_choice/run.bat
+++ Other Projects/food_choice/run.bat
@@ -1,5 +1,10 @@
 @echo off
+
+:loop
 cls
 color 0a
 py food_choice.py
+pause
+goto loop
+
 pause
